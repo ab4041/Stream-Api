f6652f69291ac0349a12b0c96a984c5297d8bc03
ab4041 <138356134+ab4041@users.noreply.github.com>
2026-04-15 19:50:01 +0530

    Added UC2.3 collect example

---
+++ src/main/java/com/streamapi/UC2_3_CollectList.java
@@ -0,0 +1,21 @@
+package com.streamapi;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UC2_3_CollectList {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers =
+                Arrays.asList(1,2,3,4,5);
+
+        List<Integer> doubledList =
+                numbers.stream()
+                        .map(n -> n * 2)
+                        .collect(Collectors.toList());
+
+        System.out.println(doubledList);
+    }
+}
